3e2735414ab2968319dd0ee76bf09c397c913941
vazelinc4ik <issachannel@gmail.com>
2025-02-19 21:47:42 +0300

    Fixed alembic models problems

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,6 @@
 from .base import Base
 from .stores import Stores
 from .tv_details import TVDetails
+from .dns_table import DNSTable
+from .regions import Regions
+from .static_info import StaticInfo
--- models/tv_details.py
+++ models/tv_details.py
@@ -21,5 +21,5 @@ class TVDetails(Base):
     point_number: Mapped[int] = mapped_column(Integer, nullable=False)
     
     store_info: Mapped["Stores"] = relationship("Stores", back_populates="tv_details") #type: ignore
-    static_info = Mapped["StaticInfo"] = relationship("StaticInfo", back_populates="tv_detail", lazy="joined") #type: ignore
+    static_info: Mapped["StaticInfo"] = relationship("StaticInfo", back_populates="tv_detail", lazy="joined") #type: ignore
     
